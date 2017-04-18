aa4d478015fc0df7453ffb1c29a3b3d05f600ead
hjsiso <hjsiso@gmail.com>
2017-04-18 15:05:07 -0400

    commit 2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <v-container fluid="fluid">
-    <v-row class="mt-1 ml-1 mr-1">
-      <v-col xs12>
-        <v-card class="primary mt-2">
-          <v-card-text>12</v-card-text>
-        </v-card>
+    <v-row class="mt-4 ml-1 mr-1">
+      <v-col xs6>
+        <!--<v-card class="primary mt-2">
+          <v-card-text>-->
+          <div class="ml-5 mt-2">
+            <div>
+              <div>Membership name</div><strong>Rol user details</strong></div>
+          </div>
+
+        <!--  </v-card-text>
+        </v-card>-->
+      </v-col>
+      <v-col xs6>
+        <!--<v-card class="primary mt-2">
+          <v-card-text>-->
+          <div class="ml-5 mt-2">
+            <div><v-btn round primary dark>Rounded Button</v-btn><v-btn round primary dark>Rounded Button</v-btn></div>
+          </div>
+
+        <!--  </v-card-text>
+        </v-card>-->
       </v-col>
       <v-col xs6 v-for="i in 2" :key="i">
         <v-card class="secondary mt-2">
