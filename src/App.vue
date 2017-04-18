fd4af77737de3e689f6fbcb937f669fc0eddb532
hjsiso <hjsiso@gmail.com>
2017-04-18 14:43:41 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container fluid="fluid">
+    <v-row class="mt-1 ml-1 mr-1">
+      <v-col xs12>
+        <v-card class="primary mt-2">
+          <v-card-text>12</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col xs6 v-for="i in 2" :key="i">
+        <v-card class="secondary mt-2">
+          <v-card-text>6</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col xs4 v-for="i in 3" :key="i">
+        <v-card class="primary mt-2">
+          <v-card-text>4</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col xs3 v-for="i in 4" :key="i">
+        <v-card class="secondary mt-2">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col xs2 v-for="i in 6" :key="i">
+        <v-card class="primary mt-2">
+          <v-card-text>2</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col xs1 v-for="i in 12" :key="i">
+        <v-card class="secondary mt-2">
+          <v-card-text>1</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        item: {
+          text: 'Get Started'
+        },
+        nav2: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import '../node_modules/vuetify/src/stylus/main'
+
+  @import './css/main.css'
+</style>
